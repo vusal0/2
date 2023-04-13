2e0c5bd9ed668170130c04939c975f86f94e78cb
vusal0 <778uu72@code.edu.az>
2023-04-13 12:21:15 +0400

    alqoritm

---
+++ main.js
@@ -0,0 +1,54 @@
+// Conditions 2
+// var dil = prompt();
+// switch (dil) {
+//         case "a":
+//             alert("Azerbaycan dili secildi");
+//             break;
+//         case "r":
+//             alert("Rus dili secildi");
+//             break;
+//         case "e":
+//             alert("Ingilis dili secildi");
+//             break;
+//         case "i":
+//             alert("Ispan dili secildi");
+//             break;
+//         case "f":
+//             alert("Fransiz dili secildi");
+//             break;
+//         case "t":
+//             alert("Turk dili secildi");
+//             break;
+//         default:
+//             alert("yanlis sorgu");
+//             break;
+//     }
+
+// Loops 2
+// var num;
+// do{
+//     num = prompt("Ededi daxil edin")
+// }
+// while(isNaN(num))
+// alert(num % 2 == 0 ? "Cutdur":"Tekdir")
+// var num
+// num = prompt("Ededi daxil edin")
+// while(isNaN(num)){
+//     alert("Eded daxil et");
+//     num = prompt("Ededi daxil edin")
+// }
+
+// Loops 1
+// var number = prompt("Eded daxil edin: ");
+// if(number > 99 && number < 1000){
+//     var teklik = number % 10;
+//     var onluq = ((number - teklik) / 10) % 10;
+//     var a = (onluq * 10) + teklik;
+//     var yuzluk = ((number - a) / 100) % 10;
+
+//     var sum = teklik + onluq + yuzluk;
+//     alert(sum);
+// }
+// else{
+//     alert("Duzgun daxil edin!");
+// }
